8d86a65a497f62a45520ecfe9d5f2a95ea9a4a7f
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-04-18 12:30:57 +0200

    clase 18/04 programación

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // SuperHero bueno = new SuperHero("el bueno", 5, 5, 5, 5);
+        // SuperHero malo = new SuperHero("el malo", 5, 5, 5, 5);
+        
+        // System.out.println("Gana el bueno?: "+ bueno.combate(malo));
+        // System.out.println("Gana el malo?: "+malo.combate(bueno));
+
+        SuperHero heroe1 = new SuperHero();
+        SuperHero hero2 = new SuperHero("inteligencia");
     }
 }
