ff40e793fd278146c0a63b771f4d872c5ffc87d8
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-04-18 09:51:13 +0200

    inicio practicasBase

---
+++ src/SuperHero.java
@@ -0,0 +1,18 @@
+public class SuperHero {
+   // atributos:
+      // nombre como string
+      // caracteristicas (de tipo numérico): 
+            // inteligencia, agilidad, fuerza, resistencia.
+   // constructor
+      // con todos los parametros.
+
+   // getters y setters
+   // toString (de VSC)
+   // métodos:
+   // public void entrenamiento(int dias,String caracteristica)
+   // lo q hace es la caracteristica la incrementa en funcion del número de días
+   // introducido pero que genere un valor aleatorio entre  0 y esos días y ese valor
+   // se le suma a la característica.
+   // ejemplo: entrenamiento(5,"agilidad");
+   // me incrementa agilidad en un valor=[0..5](aleatorio)
+}
